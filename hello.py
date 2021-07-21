5989b87b6ad2683fb5344af07619e061f680434b
sergeyyavorskiy <44283438+sergeyyavorskiy@users.noreply.github.com>
2021-07-21 14:18:45 +0600

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,6 +1,22 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Terraform!'
+application = Flask(__name__)
+
+
+@application.route("/")
+def root():
+    return render_template("index.html")
+
+@application.route("/help")
+def helppage():
+    return render_template("help.html")
+
+@application.route("/hello")
+def index():
+    return "Hello World from Flask Hello Page.<b> v1.0"
+
+#--------Main------------------
+if __name__ == "__main__":
+    application.debug = True
+    application.run()
+#------------------------------
