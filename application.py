c48de1156afbd83d784d19e2a1f55e742ab56069
sergeyyavorskiy <44283438+sergeyyavorskiy@users.noreply.github.com>
2021-07-21 14:54:01 +0600

    Update application.py

--- application.py
+++ application.py
@@ -1,7 +1,5 @@
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+from flask import Flask 
+application = Flask(__name__)
+@application.route('/') 
+def hello_world(): 
+    return 'Hello GitHub Actions World!'
